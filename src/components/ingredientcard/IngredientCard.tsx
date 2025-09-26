f37ab1a2846b8bdbf2a307dbf37d6e73cbf6b2b0
Seyone Gunasingham <s.g.seyone@proton.me>
2025-09-26 22:39:46 +0530

    Better looking cards

--- src/components/ingredientcard/IngredientCard.tsx
+++ src/components/ingredientcard/IngredientCard.tsx
@@ -1,21 +1,37 @@
 import styles from "./IngredientCard.module.css";
-import {IIngredientData} from "@/models/Ingredient";
+import { IIngredientData } from "@/models/Ingredient";
 
-export default function IngredientCard({ingredient}: { ingredient: IIngredientData }) {
+export default function IngredientCard({ ingredient }: { ingredient: IIngredientData }) {
     return (
-        <li key={ingredient.name} className={styles.card}>
-            <h2 className={styles.ingName}>{ingredient.name}</h2>
-            <p>
-                <strong>Provenance:</strong> {ingredient.provenance || "Unknown"}
-            </p>
-            {ingredient.flavor_profile && (
-                <p>
-                    <strong>Flavor:</strong> {ingredient.flavor_profile.join(", ")}
-                </p>
-            )}
-            {ingredient.comment && (
-                <p dangerouslySetInnerHTML={{__html: ingredient.comment}}></p>
-            )}
+        <li className={styles.card}>
+            <div className={styles.content}>
+                <div className={styles.header}>
+                    <h2 className={styles.ingName}>{ingredient.name}</h2>
+                    {ingredient.pronunciation && (
+                        <span className={styles.pronunciation}>/{ingredient.pronunciation}/</span>
+                    )}
+                </div>
+
+                {ingredient?.country && ingredient?.cuisine && (ingredient?.country?.length > 0 || ingredient.cuisine?.length > 0) && (
+                    <p className={styles.meta}>
+                        <strong>Country: </strong>{ingredient.country?.join(", ")}{" | "}
+                        <strong>Cuisine: </strong>{ingredient.cuisine?.length > 0 && <>· {ingredient.cuisine.join(", ")}</>}
+                    </p>
+                )}
+
+                {ingredient.flavor_profile && ingredient.flavor_profile?.length > 0 && (
+                    <p className={styles.flavor}>
+                        <strong>Flavor:</strong> {ingredient.flavor_profile.join(", ")}
+                    </p>
+                )}
+
+                {ingredient.comment && (
+                    <p
+                        className={styles.comment}
+                        dangerouslySetInnerHTML={{ __html: ingredient.comment }}
+                    />
+                )}
+            </div>
         </li>
     );
 }
